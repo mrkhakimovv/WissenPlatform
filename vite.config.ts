2602d4de45be65268314e898dab7df640d26a733
mrkhakimovv <khakimovvv2107@gmail.com>
2026-08-16 23:52:50 +0500

    chore: update app icons and add sharp dependency

--- vite.config.ts
+++ vite.config.ts
@@ -21,13 +21,13 @@ export default defineConfig(({mode}) => {
           display: 'standalone',
           icons: [
             {
-              src: 'logo.png',
+              src: 'logo-192.png',
               sizes: '192x192',
               type: 'image/png',
               purpose: 'any'
             },
             {
-              src: 'logo.png',
+              src: 'logo-512.png',
               sizes: '512x512',
               type: 'image/png',
               purpose: 'any maskable'
